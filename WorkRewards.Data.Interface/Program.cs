4c772060d49e2e3159c399e0975045c5d70ef455
phani1102 <phani1102@gmail.com>
2021-01-30 11:24:00 +0530

    Adding dependent projects

---
+++ WorkRewards.Data.Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkRewards.Data.Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
